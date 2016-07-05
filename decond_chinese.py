ca8fec77fe9ab4d475f73331f82b39b40ffa354b
hardy1213 <fengjikun1987@163.com>
2016-07-05 12:50:55 +0800

    add email send code

--- decond_chinese.py
+++ decond_chinese.py
@@ -30,4 +30,42 @@ driver.find_element_by_id("kw").send_keys(Keys.ENTER)
 #退出浏览器
 driver.quit()
 
-print 'hello world'
+#如何发送邮件？
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+#定义发送邮箱的服务器
+smtpserver = 'smtp.163.com'
+
+#定义发送邮箱的用户名、密码
+user = 'fengjikun1987@163.com'
+password = 'fengJK1986526'
+
+#发送的邮箱
+sender = 'fengjikun1987@163.com'
+
+#定义接受的邮箱，第一种方法：列表接收
+receiver = ['fengjikun1987@163.com','fengjikun@wywk.c']
+
+#第二种方法：邮件存到TXT文件中，直接修改
+'''
+receiver = open('D:\\b.txt','r')
+lines = receiver.readlines() #每次读取一行
+receiver = [] #定义一个空列表
+for line in lines:
+    receiver.append(line.replace("\n","")) #拼接
+ '''   
+#发送邮件主题
+subject =  'python email test'
+
+#发送邮件的正文
+msg = MIMEText('<html><h1>test python</h1></html>','html','utf-8')
+msg['Subject'] = Header(subject,'utf-8')
+                                 
+#连接发送邮件
+smtp = smtplib.SMTP()   
+smtp.connect(smtpserver)
+smtp.login(user, password)
+smtp.sendmail(sender, receiver, msg.as_string())
+smtp.quit()
