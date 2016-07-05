29fa74b6d728dd49ec545cf603878002abeb8ecc
hardy1213 <fengjikun1987@163.com>
2016-07-05 13:10:46 +0800

    ss

--- decond_chinese.py
+++ decond_chinese.py
@@ -69,3 +69,40 @@ smtp.connect(smtpserver)
 smtp.login(user, password)
 smtp.sendmail(sender, receiver, msg.as_string())
 smtp.quit()
+
+#如何发送带附件的邮件
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+#发送邮箱的服务器
+smtpserver = 'smtp.163.com'
+
+#发送邮箱的用户名、密码
+user = 'fengjikun1987@163.com'
+password = 'fengJK1986526' #此密码不是邮箱密码，是邮箱代理的密码
+
+#发送的邮箱
+sender = 'fengjikun1987@163.com'
+
+#接收的邮箱，具体方法参考上面发送邮箱的方法
+receiver = ['fengjikun1987@163.com','fengjikun@wywk.c']
+
+#发送邮件主题
+subject = 'Python send attach email'
+
+#发送的附件 sendfile = open("wangyu.jpg", 'rb').read()如果不写路径，默认为当前的路径下
+sendfile = open("D:\\selenium.png",'rb').read()
+
+att = MIMEText(sendfile, 'base64', 'utf-8')
+att["Content-Type"] = 'application/octet-stream'
+att["Content-Disposition"] = 'attachment; filename="Selenium.png"'
+msgRoot = MIMEMultipart('related')
+msgRoot['Subject'] = subject
+msgRoot.attach(att)
+# 连接发送邮件
+smtp = smtplib.SMTP()
+smtp.connect(smtpserver)
+smtp.login(user, password)
+smtp.sendmail(sender, receiver, msgRoot.as_string())
+smtp.quit()
